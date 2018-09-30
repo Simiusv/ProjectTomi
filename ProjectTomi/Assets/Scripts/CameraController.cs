edb06c799413742037437ce78cfb65b696bee565
Simiusv <31772511+Simiusv@users.noreply.github.com>
2018-09-30 01:18:35 -0400

    Initial Configurations
    Left click move implemented.
    Camera Locked to player. No camera controls.

---
+++ ProjectTomi/Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject Player;
+    public Vector3 cameraOffset;
+
+    private void Start()
+    {
+
+        cameraOffset = transform.position - Player.transform.position;
+
+    }
+
+    void LateUpdate ()
+    {
+        transform.position = Player.transform.position + cameraOffset;
+        
+		
+	}
+}
