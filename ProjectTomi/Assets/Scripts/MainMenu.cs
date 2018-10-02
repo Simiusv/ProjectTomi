46ce6c62a005438208f469a4d0ef8a8b7476166f
Simiusv <31772511+Simiusv@users.noreply.github.com>
2018-10-02 13:33:01 -0400

    Main Menu
    Created a main menu scene and script to allow it to actually load another scene. New Game loads SampleScene (which is the first scene) and Load Game loads Dungeon01. Settings does nothing and Quit will exit the application.

---
+++ ProjectTomi/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+	
+	public void NewGame ()
+    {
+        //This should play an opening cutscene once we figure out how to do that
+        SceneManager.LoadScene(1);
+	}
+
+    public void LoadGame ()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void Settings ()
+    {
+
+    }
+
+    public void QuitGame ()
+    {
+        Application.Quit();
+    }
+
+}
